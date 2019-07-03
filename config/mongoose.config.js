1aaa104c4e1cafbc8da4d8daaa3481a516492654
Hamza Javaid <hhumza673@gmail.com>
2019-07-03 16:07:09 +0500

    procfile added for heroku

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose => {
   mongoose
-    .connect(process.env.DB_HOST, { useNewUrlParser: true })
+    .connect('mongodb://127.0.0.1:27017/testdb', { useNewUrlParser: true })
     .then(() => {
       console.log('Mongo DB Connected');
     })
